8a608ac5ead4ecd9a919181ebff8f6d07b682125
ist1113780 <francisco.r.simoes@tecnico.ulisboa.pt>
2026-03-09 23:29:38 +0000

    fix info and left space

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,9 +1,10 @@
 function Header() {
   return (
     <header className="header">
-      <h1>Francisco Simões</h1>
+      <h1>FRANCISCO SIMÕES</h1>
       <div className="intro-list">
-        <p>2nd Computer Science student at Instituto Superior Técnico in Lisbon, Portugal</p>
+        <p>2nd year Computer Science and Engineering Student at Instituto Superior Técnico</p>
+        <p>Lisbon, Portugal</p>
         <p>franciscojrs210@gmail.com</p>
       </div>
     </header>
